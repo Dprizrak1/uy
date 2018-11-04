c0c453dd02202c05c95020d4925fe7920443d060
émoussé <44740719+Dprizrak1@users.noreply.github.com>
2018-11-04 10:05:05 +0400

    Create project

---
+++ Assets/scripts/move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    
+    [Range(1,50)] // создаём ползунок и указываем его диапозон
+    public int speed; // создаём переменную типа int к которой будет привязан ползунок
+
+    void FixedUpdate ()
+    { //FixedUpdate вызывается с частотой фиксированных кадров (указываются в настройках проекта)
+        float move = Input.GetAxisRaw("Horizontal") * (float)speed * Time.deltaTime; //отдельно рассчитываем расстояяние на которое будет перемещён гг
+        transform.Translate(move, 0, 0); //функция Translate перемещает объект в направление и на расстояние от заданных координат x, y и z соответственно 
+        Debug.Log("DeltaTime: "+Time.deltaTime);
+	}
+}
